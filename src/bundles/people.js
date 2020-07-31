5a9546f17edacd6a2624ec3df29f35c86ad157b9
brian.kotek <kotek_brian@bah.com>
2020-07-31 09:52:34 -0400

    Add some debug logging

--- src/bundles/people.js
+++ src/bundles/people.js
@@ -16,17 +16,22 @@ export const PeopleActions = {
   FETCH_PEOPLE_ERROR: "FETCH_PEOPLE_ERROR"
 };
 
+const debugLog = true;
+
 export default {
   name: 'people',
 
   getReducer: () => {
-    const initialData = {
+    const initialState = {
       data: null,
-      loading: false
-    }
+      loading: false,
+      lastError: null,
+      lastFetch: null
+    };
 
-    return ( state = initialData, { type, payload } ) => {
+    return ( state = initialState, { type, payload } ) => {
       if( type === PeopleActions.FETCH_PEOPLE_START ) {
+        if( debugLog ) console.log( "people bundle: reducer handling FETCH_PEOPLE_START" );
         return Object.assign( {}, state, {
           loading: true
         } )
@@ -38,6 +43,7 @@ export default {
         } )
       }
       if( type === PeopleActions.FETCH_PEOPLE_SUCCESS ) {
+        if( debugLog ) console.log( "people bundle: reducer handling FETCH_PEOPLE_SUCCESS" );
         return Object.assign( {}, state, {
           loading: false,
           lastFetch: Date.now(),
@@ -59,21 +65,26 @@ export default {
   },
 
   doFetchPeople: () => ( { dispatch, swapiFetch } ) => {
-    dispatch( { type: PeopleActions.FETCH_PEOPLE_START } )
+    if( debugLog ) console.log( "people bundle: doFetchPeople()" );
+    dispatch( { type: PeopleActions.FETCH_PEOPLE_START } );
     swapiFetch( '/people' )
       .then( payload => {
         dispatch( { type: PeopleActions.FETCH_PEOPLE_SUCCESS, payload } )
       } )
       .catch( error => {
         dispatch( { type: PeopleActions.FETCH_PEOPLE_ERROR, error } )
-      } )
+      } );
   },
 
-  peopleBundle_selectPeopleRaw: state => state.people,
+  selectPeopleRaw: state => {
+    if( debugLog ) console.log( "people bundle: selectPeopleRaw()" );
+    return state.people;
+  },
 
-  selectPeopleRaw: state => state.people,
-
-  selectPeopleData: state => state.people.data,
+  selectPeopleData: state => {
+    if( debugLog ) console.log( "people bundle: selectPeopleData()" );
+    return state.people.data;
+  },
 
   selectActivePerson: createSelector(
     'selectRouteParams',
@@ -83,16 +94,25 @@ export default {
       if( !pathname.includes( '/people' ) || !routeParams.id || !peopleData ) {
         return null
       }
+      if( debugLog ) console.log( "people bundle: selectActivePerson()" );
       return peopleData.find( person => person.id === routeParams.id ) || null
     }
   ),
 
-  reactShouldFetchPeople: createSelector( 'selectPeopleRaw', peopleData => {
-    if( peopleData.loading || peopleData.data ) {
-      return false
+  // BK: This gets run automatically by redux-bundler but I'm not sure how it decides when to trigger it.
+  // Nothing in the app calls it directly. My *guess* is that it sets itself up so any time something tries to call
+  // `selectPeopleRaw`, it automatically runs this and if needed it triggers the action creator.
+  // The docs emphasize that this MUST change the condition it is using or it can trigger an infinite loop.
+  // In this case, triggering `doFetchPeople` sets loading to true and eventually populates `people`, so subsequent
+  // calls to this will return false.
+  reactShouldFetchPeople: createSelector(
+    'selectPeopleRaw',
+    peopleData => {
+      if( peopleData.loading || peopleData.data ) return false;
+      if( debugLog ) console.log( "people bundle: reactShouldFetchPeople() triggering `doFetchPeople`" );
+      return { actionCreator: 'doFetchPeople' };
     }
-    return { actionCreator: 'doFetchPeople' }
-  } ),
+  ),
 
   // we'll extract a status string here, for display, just to show
   // the type of information available about the data
